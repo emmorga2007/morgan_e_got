0761cb1e0a0cf0022669607ed434a6f701d057f4
Emily Morgan <you@example.com>
2020-02-28 11:46:33 -0500

    added video

--- GOT_video/js/GOT_master.js
+++ GOT_video/js/GOT_master.js
@@ -2,6 +2,7 @@
   //variables at the top -> elements on the page we need to work with
   let sigilButtons = document.querySelectorAll('.sigilContainer');
       lightBox = document.querySelector('.lightbox');
+      gotVideo = lightBox.querySelector('video')
       closeLightBox = lightBox.querySelector('.lightbox-close')
 
   function showLightbox() {
@@ -9,16 +10,21 @@
     //start with a video
 
     lightBox.classList.add('show-lightbox');
+
+    gotVideo.play()
   }
 
-  function  closeLightBox(){
-    lightBox.classList.remove('show-lightbox')
+  function  hideLightBox(){
+    lightBox.classList.remove('show-lightbox');
+
+    gotVideo.pause();
+    gotVideo.currentTime = 0;
   }
 
   /// add a click event to the sigilButtons
-  sigilButtons.forEach(button => button.addEventListener('click', showLightbox))
+  sigilButtons.forEach(button => button.addEventListener('click', showLightbox));
 
   // add an event listener for the light box close buttons
-  closeLightBox.addEventListener('click', hideLightbox)
+  closeLightBox.addEventListener('click', hideLightBox);
 
 })();
